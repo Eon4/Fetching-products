7721060666a90fe4f8423e16479595e7b7ec3907
Eon4 <ladyforest85@gmail.com>
2023-04-18 10:40:20 +0200

    update

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+function getProducts() {
+    return fetch("https://dummyjson.com/products?limit=8")
+      .then((response) => response.json())
+      .then((data) => data)
+      .catch((error) => console.error(error));
+  }
+  
+  // const containerElement = document.querySelector(".product-container");
+  const containerElement = document.querySelector("#products");
+  
+  function addProductTooDom(products) {
+    const productElement = document.createElement("div");
+  
+    productElement.classList.add("product");
+  
+    productElement.innerHTML = `
+    <header>
+            <h2 class="product-title">${products.title}</h2>
+            <p class="product-description">${products.description}</p>
+          </header>
+          <img
+            class="product-img"
+            src="${products.thumbnail}"
+            alt="${products.title}"
+          />
+  
+          <section class="purshare">
+            <p class="product-price">$${products.price}</p>
+            <a class="add-to-cart" href="index.html">Add to cart</a>
+          </section>
+    
+    
+    `;
+  
+    containerElement.appendChild(productElement);
+  }
+  
+  getProducts().then((data) => {
+    console.log(data);
+    data.products.map((product) => {
+      addProductTooDom(product);
+    });
+  });
+    
